4fac38f400e6079dbf96f0ddc618bdf1609bb9f2
khanian <khanylee@gmail.com>
2025-07-11 14:30:26 +0900

    modify test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { useState } from "react";
 
 function FilterableProductTable({ products }) {
   const [filterText, setFilterText] = useState('');
-  const [inStockOnly, setInstockOnly] = useState(false);
+  const [inStockOnly, setInStockOnly] = useState(false);
   return (
     <div>
       <SearchBar 
         filterText={filterText} 
-        inStockOnly={inStockOnly} />
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}
+      />
       <ProductTable 
         products={products} 
         filterText={filterText} 
@@ -40,11 +43,16 @@ function ProductRow({ product }) {
   );
 }
 
-function ProductTable({ products }) {
+function ProductTable({ products, filterText, inStockOnly }) {
   const rows = [];
   let lastCategory = null;
 
   products.forEach((product) => {
+    // if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1)
+    if (!product.name.toLowerCase().includes(filterText.toLowerCase()))
+      return;
+    if (inStockOnly && !product.stocked) 
+      return;
     if (product.category !== lastCategory) {
       rows.push(
         <ProductCategoryRow
@@ -73,12 +81,25 @@ function ProductTable({ products }) {
   );
 }
 
-function SearchBar() {
+function SearchBar({ filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange }) {
   return (
     <form>
-      <input type="text" placeholder="Search..." />
+      <input 
+        type="text"
+        value={filterText} 
+        placeholder="Search..."
+        onChange={e=>
+          onFilterTextChange(e.target.value || '')
+        }
+      />
       <label>
-        <input type="checkbox" />
+        <input 
+          type="checkbox" 
+          checked={inStockOnly}
+          onChange={e=>
+            onInStockOnlyChange(e.target.checked)
+          }
+        />
         {' '}
         Only show products in stock
       </label>
