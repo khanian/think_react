78e89fd30fef4b133b3b698414aed9a1077d8e0e
khanian <khanylee@gmail.com>
2025-07-11 11:01:58 +0900

    add test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-<!DOCTYPE html>
-<html>
-<body>
-  <div id="root"></div>
-</body>
-<!-- This setup is not suitable for production. -->
-<!-- Only use it in development! -->
-<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
-<script async src="https://ga.jspm.io/npm:es-module-shims@1.7.0/dist/es-module-shims.js"></script>
-<script type="importmap">
-{
-  "imports": {
-    "react": "https://esm.sh/react?dev",
-    "react-dom/client": "https://esm.sh/react-dom/client?dev"
-  }
+import { useState } from "react";
+
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInstockOnly] = useState(false);
+  return (
+    <div>
+      <SearchBar 
+        filterText={filterText} 
+        inStockOnly={inStockOnly} />
+      <ProductTable 
+        products={products} 
+        filterText={filterText} 
+        inStockOnly={inStockOnly}/>
+    </div>
+  );
 }
-</script>
-<script type="text/babel" data-type="module">
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 
 function ProductCategoryRow({ category }) {
   return (
@@ -89,15 +86,6 @@ function SearchBar() {
   );
 }
 
-function FilterableProductTable({ products }) {
-  return (
-    <div>
-      <SearchBar />
-      <ProductTable products={products} />
-    </div>
-  );
-}
-
 const PRODUCTS = [
   {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
   {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
@@ -107,82 +95,6 @@ const PRODUCTS = [
   {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
 ];
 
-let App = function App() {
+export default function App() {
   return <FilterableProductTable products={PRODUCTS} />;
 }
-
-
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
-</script>
-<style>
-* {
-  box-sizing: border-box;
-}
-
-body {
-  font-family: sans-serif;
-  margin: 20px;
-  padding: 0;
-}
-
-h1 {
-  margin-top: 0;
-  font-size: 22px;
-}
-
-h2 {
-  margin-top: 0;
-  font-size: 20px;
-}
-
-h3 {
-  margin-top: 0;
-  font-size: 18px;
-}
-
-h4 {
-  margin-top: 0;
-  font-size: 16px;
-}
-
-h5 {
-  margin-top: 0;
-  font-size: 14px;
-}
-
-h6 {
-  margin-top: 0;
-  font-size: 12px;
-}
-
-code {
-  font-size: 1.2em;
-}
-
-ul {
-  padding-inline-start: 20px;
-}
-
-body {
-  padding: 5px;
-}
-label {
-  display: block;
-  margin-top: 5px;
-  margin-bottom: 5px;
-}
-th {
-  padding-top: 10px;
-}
-td {
-  padding: 2px;
-  padding-right: 40px;
-}
-
-</style>
-</html>
